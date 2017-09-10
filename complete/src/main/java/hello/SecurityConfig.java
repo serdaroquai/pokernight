2809b2ed4c34e6d8718cd9bdb019a09f4d1e0844
serdar <baykan.serdar@gmail.com>
2017-09-10 15:53:56 +0300

    added a few more players

--- complete/src/main/java/hello/SecurityConfig.java
+++ complete/src/main/java/hello/SecurityConfig.java
@@ -24,6 +24,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			.inMemoryAuthentication()
 				.withUser("player1").password("player1").roles("USER").and()
 				.withUser("player2").password("player2").roles("USER").and()
+				.withUser("player3").password("player3").roles("USER").and()
+				.withUser("player4").password("player4").roles("USER").and()
+				.withUser("player5").password("player5").roles("USER").and()
+				.withUser("player6").password("player6").roles("USER").and()
+				.withUser("player7").password("player7").roles("USER").and()
+				.withUser("player8").password("player8").roles("USER").and()
 				.withUser("observer").password("observer").roles("USER");
 	}
 }
